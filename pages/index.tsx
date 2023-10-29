f2d4630b6c614199a4d560f28389ca6f0aecedec
yanxiaosong0902 <xiaosongkeith@163.com>
2023-10-29 14:36:57 +0800

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,11 @@ export const getServerSideProps: GetServerSideProps<{ time: string }> = async ({
 }) => {
   res.setHeader(
     'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59'
+    'public, s-maxage=30, stale-while-revalidate=59'
+  )
+  res.setHeader(
+    'yanxiaosong',
+    'hahahahah'
   )
 
   return {
